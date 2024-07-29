c46c3fe22e8c38f4b8aec7a7464b5c9bc06394a1
Joao Leonardo Pereira <jlcvp@users.noreply.github.com>
2024-07-29 07:13:09 -0300

    componentize and update style

--- src/app/components/account-detail/account-detail.component.ts
+++ src/app/components/account-detail/account-detail.component.ts
@@ -1,14 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Account2FA } from 'src/app/models/account2FA.model';
+import { OtpService } from 'src/app/services/otp.service';
 
 @Component({
   selector: 'app-account-detail',
   templateUrl: './account-detail.component.html',
   styleUrls: ['./account-detail.component.scss'],
 })
-export class AccountDetailComponent  implements OnInit {
+export class AccountDetailComponent {
 
-  constructor() { }
+  private timerRefreshInterval: any
+  private _account?: Account2FA
+  timer: number = 0 
+  token = '000000'
 
-  ngOnInit() {}
+  @Input() set account(value: Account2FA | undefined) {
+    this._account = value
+    this.updateCode()
+    this.updateTimer()
+  }
+  get account(): Account2FA | undefined {
+    return this._account
+  }
 
+  constructor(private otpService: OtpService, private toastController: ToastController) { }
+
+  async copyCode(evt: any) {
+    if(!this.account) {
+      return
+    }
+    const code = this.token
+    await navigator.clipboard.writeText(code)
+    const toast = await this.toastController.create({
+      message: `Código copiado`,
+      positionAnchor: evt.target,
+      cssClass: 'width-auto',
+      duration: 2000
+    })
+    console.log({evt})
+    await toast.present()
+  }
+
+  updateTimer() {
+    if(this.timerRefreshInterval) {
+      clearInterval(this.timerRefreshInterval)
+    }
+    if(this.account) {
+      this.timer = this.account.getNextRollingTimeLeft()
+      this.timerRefreshInterval = setInterval(() => {
+        if(!this.account) {
+          clearInterval(this.timerRefreshInterval)
+          this.timer = NaN
+        } else {
+          this.timer = this.account.getNextRollingTimeLeft()
+          if (this.timer == this.account.interval) { // new code needed
+            this.updateCode()
+          }
+        }
+      }, 500) // for precision purposes update every 500ms
+    }
+  }
+
+  updateCode() {
+    if(this.account) {
+      console.log("generating new code")
+      this.token = this.otpService.generateTOTP(this.account.secret, this.account.interval)
+    }
+  }
 }
