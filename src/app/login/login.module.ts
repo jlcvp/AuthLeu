45c7c22f92847e58ede9b40554499f5136962289
Joao Leonardo Pereira <jlcvp@users.noreply.github.com>
2024-09-02 18:42:54 -0300

    i18n setup and login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { LoginPage } from './login.page';
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [LoginPage]
 })
